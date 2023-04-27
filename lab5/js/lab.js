77cdcfb34d2d4b1137e456ccc383a904fa727843
calebnoir <caleb9black@gmail.com>
2023-04-26 18:38:34 -0700

    Update lab.js

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,10 +1,23 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Lab 5: Data Types and Variables
+// Author: Caleb Black
+// Date: 4/26/23
 
-// Constants
+// Define Variables
+make = "Honda";
+model = "Civic";
+color = "Black";
+year = 2017;
 
-// Functions
+// Calculate
+age = 2023 - year;
+
+// output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
+
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
