2a025da06dd0927ab79099dcf0899acb5d2832bd
calebnoir <caleb9black@gmail.com>
2023-05-01 15:06:23 -0700

    Update lab.js

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,10 +1,26 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Lab 6 - Arrays and Objects
+// Author: Caleb Black
+// Date: 5/1/23
 
-// Constants
+// define variables
+myTransport = ["Honda Civic", "Scooter", "Bus", "Bike"];
 
-// Functions
+// create Objects aka my car attributes
+myMainRide = {
+  make: "Honda",
+  model: "Civic",
+  color: "Black",
+  year: 2017,
+  age: function() {
+      return 2023 - this.year;
+  }
+}
+
+// output
+document.writeln("The ways I get around Santa Cruz: " + myTransport, "</br>");
+// write object in document
+document.writeln("My Main Ride: <pre>",
+    JSON.stringify(myMainRide, null, '\t'), "</pre>");
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
