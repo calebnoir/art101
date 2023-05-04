822206efc6f8443270239ec0ba51fcc26bf0f87c
calebnoir <caleb9black@gmail.com>
2023-05-03 23:17:52 -0700

    Update lab.js

---
+++ lab7/js/lab.js
@@ -0,0 +1,22 @@
+// index.js - purpose and description here
+// Author: Caleb Black
+// Date: 5/3/23
+
+// Constants
+
+// Functions
+
+// this is an example function and this comment tells what it doees and what parameters are passed to it.
+function myFunction(param1, param2) {
+    // some code here
+    // return results;
+  }
+  
+  function main() {
+    console.log("Main function started.");
+    // the code that makes everything happen
+  }
+  
+  // let's get this party started
+  main();
+  
